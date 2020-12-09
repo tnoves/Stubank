7d862dc443317b42ad4e5d40675815ea8a94cb75
Harry Alexander <b9034879@newcastle.ac.uk>
2020-12-09 19:32:19 +0000

    created user object class with getter and setter methods

---
+++ app/src/main/java/user.java
@@ -0,0 +1,77 @@
+import java.util.Date;
+
+public class user {
+
+    private String firstName, lastName, email, phoneNumber, username, password;
+    private Date dob;
+
+    public user(String firstName, String lastName, String email, String phoneNumber, String username, String password, Date dob){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.username = username;
+        this.password = password;
+        this.dob = dob;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void delete(){
+
+    }
+}
