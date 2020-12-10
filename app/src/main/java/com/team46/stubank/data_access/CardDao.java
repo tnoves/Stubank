38e14b97b0ea89b3ee534ec877f7b6ce6a70bf50
George <georgecartridge@gmail.com>
2020-12-10 15:01:29 +0000

    added card data access layer with boilerplate functions

---
+++ app/src/main/java/com/team46/stubank/data_access/CardDao.java
@@ -0,0 +1,32 @@
+package com.team46.stubank.data_access;
+
+import com.team46.stubank.Card;
+
+import java.util.List;
+
+public class CardDao {
+
+    public Card getCard(String accountNumber) {
+        Card card = null;
+
+        return card;
+    }
+
+    public List<Card> getAllCards(String accountNumber) {
+        List<Card> cards = null;
+
+        return cards;
+    }
+
+    public boolean insertCard(Card card) {
+        return true;
+    }
+
+    public boolean updateCard(Card card) {
+        return true;
+    }
+
+    public boolean deleteCard(Card card) {
+        return true;
+    }
+}
