d680dfe1a374d1098366c0179e87096256dc6060
Harry Alexander <b9034879@newcastle.ac.uk>
2020-12-11 10:49:55 +0000

    added boilerplate code to user DAO class

---
+++ app/src/main/java/com/team46/stubank/userDAO.java
@@ -0,0 +1,23 @@
+package com.team46.stubank;
+
+public class userDAO {
+
+    private int id;
+    private user user;
+
+    public int getUser(int user){
+        return user;
+    }
+
+    public int[] getUsers(int[] users){
+        return users;
+    }
+
+    public void insertUser(user user) {
+
+    }
+
+    public void DeleteUser(user user){
+
+    }
+}
