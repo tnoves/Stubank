52a514d6c4cc647f19770fbb00ff9793dd369f27
b9035781 <b9035781@newcastle.ac.uk>
2020-12-11 09:31:28 +0000

    added template code for paymentAccount

---
+++ app/src/main/java/com/team46/stubank/PaymentAccount.java
@@ -0,0 +1,39 @@
+package com.team46.stubank;
+
+public class PaymentAccount {
+
+    private String firstName;
+    private String lastName;
+    private Double accountNumber;
+    private String sortCode;
+
+
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setFirstName(String firstName){
+        firstName = firstName;
+    }
+
+    public String getLastName(){
+        return  lastName;
+    }
+    public void  setLastName(String lastName){
+        lastName = lastName;
+    }
+
+    public Double getAccountNumber(){
+        return accountNumber;
+    }
+    public void setAccountNumber(Double accountNumber){
+        accountNumber = accountNumber;
+    }
+
+    public String getSortCode(){
+        return sortCode;
+    }
+    public void setSortCode(String sortCode){
+        sortCode = sortCode;
+    }
+    
+}
