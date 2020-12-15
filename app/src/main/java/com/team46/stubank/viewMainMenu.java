0637630098e67624fca3b35aa933946258a74e62
Harry Alexander <b9034879@newcastle.ac.uk>
2020-12-15 16:41:35 +0000

    connected MainActivity.java to Anthony's activity_login.xml screen so that it appears immediately when the user starts the app.
    Connected activty_main.xml to viewMainMenu.java class. it now displays a main menu screen that allows user to navigate to the card screen
    
    Made changes to AndroidManifest.xml file to allow viewMainMenu.java to be parent activty to displayCards.java activity, allowing users to return back to the main menu from cards screen (activity_display_cards.xml).
    
    created activity_display_cards.xml page
    
    added some strings to strings.xml file so to name buttons and text etc, instead of hardcoding them

--- app/src/main/java/com/team46/stubank/viewMainMenu.java
+++ app/src/main/java/com/team46/stubank/viewMainMenu.java
@@ -6,18 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class viewMainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_main);
     }
 
-    public void viewMenu(View view){
-        Intent intent = new Intent(this, viewMainMenu.class);
+    public void viewCard(View view){
+        Intent intent = new Intent(this, displayCards.class);
         startActivity(intent);
     }
-
 }
