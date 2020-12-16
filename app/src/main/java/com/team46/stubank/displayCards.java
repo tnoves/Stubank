4c13c0ce45007867dbdc0fd5f33936718b767a3c
Harry Alexander <b9034879@newcastle.ac.uk>
2020-12-16 19:32:22 +0000

    Created card layout in cards page

--- app/src/main/java/com/team46/stubank/displayCards.java
+++ app/src/main/java/com/team46/stubank/displayCards.java
@@ -1,6 +1,7 @@
 package com.team46.stubank;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +11,6 @@ public class displayCards extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_cards);
+
     }
 }
