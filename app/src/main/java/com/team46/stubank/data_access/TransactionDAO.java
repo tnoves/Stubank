2feacda1882f95a1e875895984f7d1b1164525ea
Ben McIntyre (UG) <b9005046@newcastle.ac.uk>
2021-01-15 14:39:04 +0000

    Added Create method in TransactionDAO to allow transactions to be added to database.

--- app/src/main/java/com/team46/stubank/data_access/TransactionDAO.java
+++ app/src/main/java/com/team46/stubank/data_access/TransactionDAO.java
@@ -1,5 +1,50 @@
 package com.team46.stubank.data_access;
 
+import com.google.gson.JsonObject;
+import com.team46.stubank.Transaction;
+
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class TransactionDAO {
 
+    public boolean insertTransaction(Transaction transaction) {
+        HttpURLConnection conn = null;
+        try {
+            URL url = new URL(String.format("http://127.0.0.1:5000/transaction/"));
+
+            conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; utf-8");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+
+            JsonObject json = new JsonObject();
+
+            json.addProperty("card_number", transaction.getCardNumber());
+            json.addProperty("balance", transaction.getBalanceAtTransaction());
+            json.addProperty("date", transaction.getDateTransaction().toString());
+            json.addProperty("payment_account_id", transaction.getPaymentAccountID());
+            json.addProperty("payment_amount", transaction.getAmount());
+            json.addProperty("payment_type", transaction.getPaymentType());
+
+            DataOutputStream dataOutputStream = new DataOutputStream(conn.getOutputStream());
+            dataOutputStream.writeBytes(json.toString());
+            dataOutputStream.close();
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("HttpResponseCode: " + conn.getErrorStream());
+            } else {
+                return true;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (conn != null)
+                conn.disconnect();
+        }
+    }
 }
