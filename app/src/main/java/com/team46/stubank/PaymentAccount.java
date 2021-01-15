80c5c785c9792150a53d69630264e749bb4c618e
b9035781 <b9035781@newcastle.ac.uk>
2021-01-15 02:26:48 +0000

    reverted

--- app/src/main/java/com/team46/stubank/PaymentAccount.java
+++ app/src/main/java/com/team46/stubank/PaymentAccount.java
@@ -2,30 +2,39 @@ package com.team46.stubank;
 
 public class PaymentAccount {
 
+    private String firstName;
+    private String lastName;
+    private Double accountNumber;
+    private String sortCode;
 
-    private String paymentActID;
-    private String accountID;
-    private String userDetailsID;
 
-    public String getPaymentActID(){
-        return paymentActID;
+    public String getFirstName(){
+        return firstName;
     }
-    public void setPaymentActID(String paymentActID){
-        paymentActID = paymentActID;
-    }
-
-    public String getAccountID(){
-        return accountID;
-    }
-    public void setAccountID(String accountID){
-        accountID = accountID;
+    public void setFirstName(String firstName){
+        firstName = firstName;
     }
 
-    public String getUserDetailsID(){
-        return userDetailsID;
+    public String getLastName(){
+        return  lastName;
     }
-    public void setUserDetailsID(String userDetailsID){
-        userDetailsID = userDetailsID;
+    public void  setLastName(String lastName){
+        lastName = lastName;
     }
+
+    public Double getAccountNumber(){
+        return accountNumber;
+    }
+    public void setAccountNumber(Double accountNumber){
+        accountNumber = accountNumber;
+    }
+
+    public String getSortCode(){
+        return sortCode;
+    }
+    public void setSortCode(String sortCode){
+        sortCode = sortCode;
+    }
+
 
 }
