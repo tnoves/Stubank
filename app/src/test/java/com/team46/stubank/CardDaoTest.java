fca9274b7b1baee1cc7ef44e126c54c9d6555710
George <georgecartridge@gmail.com>
2021-01-20 10:49:09 +0000

    Amended DAO tests so that all are operational

--- app/src/test/java/com/team46/stubank/CardDaoTest.java
+++ app/src/test/java/com/team46/stubank/CardDaoTest.java
@@ -8,6 +8,9 @@ import junit.framework.TestCase;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CardDaoTest extends TestCase {
     private CardDao cardDao;
     private UserDAO userDAO;
@@ -17,12 +20,12 @@ public class CardDaoTest extends TestCase {
         userDAO = new UserDAO();
         cardDao = new CardDao();
 
-        User user = userDAO.getUser(25);
+        User user = userDAO.getUser(28);
         Card card = new Card();
         card.setCardNumber("");
         card.setBalance(0);
         card.setCardType("GBP");
-        card.setAccountNum(57305520);
+        card.setAccountNum("57305520");
         card.setSortCode("112345");
         card.setCvcCode("123");
         card.setExpiryEnd("0001-01-01");
@@ -43,16 +46,73 @@ public class CardDaoTest extends TestCase {
     }
 
     @Test
-    public void testInsertCard() {
+    public void testGetAllCards() {
         userDAO = new UserDAO();
         cardDao = new CardDao();
 
-        User user = userDAO.getUser(25);
+        User user = new User();
+        user.setFirstName("cardtestfirst");
+        user.setLastName("cardtestlast");
+        user.setEmail("card@test.com");
+        user.setPhoneNumber("cardtestphone");
+        user.setUsername("card");
+        user.setPassword("cardpass");
+        user.setDob("2000-11-11");
+        userDAO.insertUser(user);
+
         Card card = new Card();
         card.setCardNumber("");
         card.setBalance(0);
         card.setCardType("GBP");
-        card.setAccountNum(57305520);
+        card.setAccountNum("57305520");
+        card.setSortCode("112345");
+        card.setCvcCode("123");
+        card.setExpiryEnd("0001-01-01");
+        card.setPaymentProcessor("Visa");
+        card.setActive(true);
+
+        cardDao.insertCard(card, user);
+
+        Card cardTwo = new Card();
+        cardTwo.setCardNumber("");
+        cardTwo.setBalance(0);
+        cardTwo.setCardType("USD");
+        cardTwo.setAccountNum("");
+        cardTwo.setSortCode("112345");
+        cardTwo.setCvcCode("123");
+        cardTwo.setExpiryEnd("0001-01-01");
+        cardTwo.setPaymentProcessor("Visa");
+        cardTwo.setActive(true);
+
+        cardDao.insertCard(cardTwo, user);
+
+        try {
+            Card newCard = cardDao.getCard(card.getCardNumber());
+
+            ArrayList<Card> cards = cardDao.getAllCards(user.getUserID());
+
+            Assert.assertEquals(card.getCardNumber(), newCard.getCardNumber());
+            Assert.assertEquals(2, cards.size());
+
+            cardDao.deleteCard(card);
+            cardDao.deleteCard(cardTwo);
+            userDAO.deleteUser(user);
+        } catch (final NullPointerException e) {
+            Assert.assertFalse(true);
+        }
+    }
+
+    @Test
+    public void testInsertCard() {
+        userDAO = new UserDAO();
+        cardDao = new CardDao();
+
+        User user = userDAO.getUser(28);
+        Card card = new Card();
+        card.setCardNumber("");
+        card.setBalance(0);
+        card.setCardType("GBP");
+        card.setAccountNum("57305520");
         card.setSortCode("112345");
         card.setCvcCode("123");
         card.setExpiryEnd("0001-01-01");
@@ -74,12 +134,12 @@ public class CardDaoTest extends TestCase {
         userDAO = new UserDAO();
         cardDao = new CardDao();
 
-        User user = userDAO.getUser(25);
+        User user = userDAO.getUser(28);
         Card card = new Card();
         card.setCardNumber("");
         card.setBalance(0);
         card.setCardType("GBP");
-        card.setAccountNum(57305520);
+        card.setAccountNum("57305520");
         card.setSortCode("112345");
         card.setCvcCode("123");
         card.setExpiryEnd("0001-01-01");
